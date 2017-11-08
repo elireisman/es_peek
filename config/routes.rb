236367938b064b38db79d118e2edb8c0e792b88e
elireisman <elireisman@github.com>
2017-11-08 14:43:03 -0800

    better routes.rb scoping for API data endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,29 @@
 Rails.application.routes.draw do
 
-	# /_cluster /_cat and /_nodes JSON data endpoints (pulled from ES)
-	get '/admin/health_data', to: 'admin#health_data'
-	get '/admin/state_data', to: 'admin#state_data'
-	get '/admin/indices_data', to: 'admin#indices_data'
-	get '/admin/aliases_data', to: 'admin#aliases_data'
-	get '/admin/shards_data', to: 'admin#shards_data'
-	get '/admin/segments_data', to: 'admin#segments_data'
-	get '/admin/node/stats_data/group/:group', to: 'admin#node_stats_group_data'
-	get '/admin/node/stats_data/name/:name', to: 'admin#node_stats_data'
-	get '/admin/node/stats_data/all', to: 'admin#node_stats_data'
-	get '/admin/node/names_data', to: 'admin#node_names_data'
+    # API JSON data endpoints for use by other services, including this app's HTML-rendering controllers
+    scope :api do
+        scope :v1 do
+            scope :data do
+                scope :cat do
+                    get '/health', to: 'admin#health_data'
+                    get '/state', to: 'admin#state_data'
+                    get '/indices', to: 'admin#indices_data'
+                    get '/aliases', to: 'admin#aliases_data'
+                    get '/shards', to: 'admin#shards_data'
+                    get '/segments', to: 'admin#segments_data'
+                end # /cat scope
 
-	# TODOs:
-	# (1) add setter for default ES cluster instead of "localhost:9200"
-	# (2) add params to endpoints to narrow down data returned in GETs
+                scope :node do
+                    get '/stats/group/:group', to: 'admin#node_stats_group_data'
+                    get '/stats/name/:name', to: 'admin#node_stats_data'
+                    get '/stats/all', to: 'admin#node_stats_data'
+                    get '/names', to: 'admin#node_names_data'
+                end # /node scope
+            end # /data scope
+        end # /v1 scope
+    end # /api scope
+
+    # TODOs:
+    # - add setter for default ES cluster instead of "localhost:9200"
 
 end
