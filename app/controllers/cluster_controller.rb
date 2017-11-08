5fe55ec78884f258b21e9a04760a1e32d708011e
elireisman <elireisman@github.com>
2017-11-08 10:52:22 -0800

    flesh out admin endpoints a bit more

--- app/controllers/cluster_controller.rb
+++ app/controllers/cluster_controller.rb
@@ -3,14 +3,31 @@ require 'elasticsearch/model'
 class ClusterController < ApplicationController
 	include Elasticsearch::Model
 
-	# TODO: mappings, settings etc.
+	# TODO: for actual types stored in indices, controllers will need mappings/settings/analyzer blocks etc.
 
 	def health_data
 		@health_data = __elasticsearch__.client.cluster.health
+		render :json => @health_data
 	end
 
-	def health
-		# render /_cluster/health data in HTML page
+	def state_data
+		@state_data = __elasticsearch__.client.cluster.state
+		render :json => @state_data
+	end
+
+	def indices_data
+		@indices_data = __elasticsearch__.client.cat.indices
+		render :json => @indices_data
+	end
+
+	def aliases_data
+		@aliases_data = __elasticsearch__.client.cat.aliases
+		render :json => @aliases_data
+	end
+
+	def shards_data
+		@shards_data = __elasticsearch__.client.cat.shards
+		render :json => @shards_data
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
-  get '/cluster/:cid/health_data', to: 'cluster#health_data'
+	# /_cluster and /_nodes JSON data endpoints (pulled from ES)
+	get '/cluster/health_data', to: 'cluster#health_data'
+	get '/cluster/state_data', to: 'cluster#state_data'
+	get '/cluster/indices_data', to: 'cluster#indices_data'
+	get '/cluster/aliases_data', to: 'cluster#aliases_data'
+	get '/cluster/shards_data', to: 'cluster#shards_data'
 
 end
